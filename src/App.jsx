78ac4eb6bae79db36e3a6fd945a816f9a55ec129
aartimahamune <aartimahamune5@gmail.com>
2024-09-27 20:18:51 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserData from './components/UserData';
+
+export default function App() {
+  return (
+    <div>
+      <UserData />
+    </div>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:'/HelloWorld/',
+  server: {
+    proxy: {
+      '/get-users': {
+        target: 'https://chitwanhumad.pythonanywhere.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/get-users/, '/get-users'),
+      },
+    },
+  },
+})
